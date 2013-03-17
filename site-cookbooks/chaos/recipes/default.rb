ac9f78c8997bfa0aaf08e9b3f8e7250ad90495ea
Etienne Garnier <garnier.etienne@gmail.com>
2013-03-17 18:24:34 +0100

    add deployment and processes starter scripts

--- site-cookbooks/chaos/recipes/default.rb
+++ site-cookbooks/chaos/recipes/default.rb
@@ -18,7 +18,28 @@ template "chaos.conf" do
 end
 
 # Add the deploy group (used by chaos tools)
+# Add git user to deploy group
 group "deploy" do
   system true
+  members "git"
+  append true
+  action :create
+end
+
+# Deployment script
+file "deploy" do
+  owner "git"
+  group "git"
+  mode "0755"
+  path "/srv/git/bin/deploy"
+  action :create
+end
+
+# Processes starter script
+file "starter" do
+  owner "git"
+  group "git"
+  mode "0755"
+  path "/srv/git/bin/starter"
   action :create
 end
