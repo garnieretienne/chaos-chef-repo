3b47c4933c9e130fd990bcde8b1306069f758e59
Etienne Garnier <garnier.etienne@gmail.com>
2013-04-14 15:39:03 +0200

    split chaos role into server and app_server role

--- site-cookbooks/chaos/recipes/default.rb
+++ site-cookbooks/chaos/recipes/default.rb
@@ -7,10 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Define the default ruby verion on the system
-rbenv_global "1.9.3-p392" do
-  action :create
-end
+# # Define the default ruby verion on the system
+# rbenv_global "1.9.3-p392" do
+#   action :create
+# end
 
 # Create an admin user
 admin_key = "/root/admin_key"
@@ -65,141 +65,141 @@ if Dir.exist? admin_key then
   end
 end
 
-# Add the deploy group (used by chaos tools)
-# Add git user to deploy group
-group "deploy" do
-  system true
-  members "git"
-  append true
-  action :create
-end
+# # Add the deploy group (used by chaos tools)
+# # Add git user to deploy group
+# group "deploy" do
+#   system true
+#   members "git"
+#   append true
+#   action :create
+# end
 
-# Create the bin folder (sourced by default profile on debian / ubuntu)
-directory "#{node['gitolite']['admin_home']}/bin" do
-  user "git"
-  group "git"
-  action :create
-end
+# # Create the bin folder (sourced by default profile on debian / ubuntu)
+# directory "#{node['gitolite']['admin_home']}/bin" do
+#   user "git"
+#   group "git"
+#   action :create
+# end
 
-# Create the app folder (sourced by default profile on debian / ubuntu)
-directory "#{node['chaos']['app']['dir']}" do
-  action :create
-end
+# # Create the app folder (sourced by default profile on debian / ubuntu)
+# directory "#{node['chaos']['app']['dir']}" do
+#   action :create
+# end
 
-# Deployment script
-template "deploy" do
-  path "#{node['gitolite']['admin_home']}/bin/deploy"
-  source "deploy"
-  owner "git"
-  group "git"
-  mode "0755"
-  action :create
-end
+# # Deployment script
+# template "deploy" do
+#   path "#{node['gitolite']['admin_home']}/bin/deploy"
+#   source "deploy"
+#   owner "git"
+#   group "git"
+#   mode "0755"
+#   action :create
+# end
 
-# Deployment hook
-template "post-update" do
-  path "#{node['gitolite']['admin_home']}/.gitolite/hooks/common/post-update"
-  source "post-update"
-  owner "git"
-  group "git"
-  mode "0755"
-  action :create
-end
+# # Deployment hook
+# template "post-update" do
+#   path "#{node['gitolite']['admin_home']}/.gitolite/hooks/common/post-update"
+#   source "post-update"
+#   owner "git"
+#   group "git"
+#   mode "0755"
+#   action :create
+# end
 
-# Processes starter script
-template "starter" do
-  path "#{node['gitolite']['admin_home']}/bin/starter"
-  source "starter"
-  owner "git"
-  group "git"
-  mode "0755"
-  action :create
-end
+# # Processes starter script
+# template "starter" do
+#   path "#{node['gitolite']['admin_home']}/bin/starter"
+#   source "starter"
+#   owner "git"
+#   group "git"
+#   mode "0755"
+#   action :create
+# end
 
-# Install mason and foreman gems
-execute "add gem binary path to PATH and /usr/sbin for git user" do
-  command "echo \"PATH=$(ruby -rubygems -e 'puts Gem.default_bindir'):/usr/sbin:\\$PATH\" >> #{node['gitolite']['admin_home']}/.profile"
-  cwd "#{node['gitolite']['admin_home']}"
-  user "git"
-  group "git"
-  action :run
-  not_if "cat #{node['gitolite']['admin_home']}/.profile | grep \"$(ruby -rubygems -e 'puts Gem.default_bindir')\""
-end
-rbenv_gem "foreman" do
-  action :install
-end
+# # Install mason and foreman gems
+# execute "add gem binary path to PATH and /usr/sbin for git user" do
+#   command "echo \"PATH=$(ruby -rubygems -e 'puts Gem.default_bindir'):/usr/sbin:\\$PATH\" >> #{node['gitolite']['admin_home']}/.profile"
+#   cwd "#{node['gitolite']['admin_home']}"
+#   user "git"
+#   group "git"
+#   action :run
+#   not_if "cat #{node['gitolite']['admin_home']}/.profile | grep \"$(ruby -rubygems -e 'puts Gem.default_bindir')\""
+# end
+# rbenv_gem "foreman" do
+#   action :install
+# end
 
-# Install chaos route manager gem (move to its own recipe)
-directory "#{node['gitolite']['admin_home']}/build" do
-  user "git"
-  group "git"
-  action :create
-end
-git "hermes source" do
-  repository "git://github.com/garnieretienne/chaos_hermes.git"
-  destination "#{node['gitolite']['admin_home']}/build/hermes"
-  user "git"
-  group "git"
-  action :checkout
-end
-execute "build hermes gem" do
-  command "gem build chaos_hermes.gemspec"
-  cwd "#{node['gitolite']['admin_home']}/build/hermes"
-  user "git"
-  group "git"
-  action :run
-  not_if "ls #{node['gitolite']['admin_home']}/build/hermes/chaos_hermes-0.0.1.gem"
-end
-rbenv_gem "hermes" do
-  source "#{node['gitolite']['admin_home']}/build/hermes/chaos_hermes-0.0.1.gem"
-  version "0.0.1"
-  action :install
-end
+# # Install chaos route manager gem (move to its own recipe)
+# directory "#{node['gitolite']['admin_home']}/build" do
+#   user "git"
+#   group "git"
+#   action :create
+# end
+# git "hermes source" do
+#   repository "git://github.com/garnieretienne/chaos_hermes.git"
+#   destination "#{node['gitolite']['admin_home']}/build/hermes"
+#   user "git"
+#   group "git"
+#   action :checkout
+# end
+# execute "build hermes gem" do
+#   command "gem build chaos_hermes.gemspec"
+#   cwd "#{node['gitolite']['admin_home']}/build/hermes"
+#   user "git"
+#   group "git"
+#   action :run
+#   not_if "ls #{node['gitolite']['admin_home']}/build/hermes/chaos_hermes-0.0.1.gem"
+# end
+# rbenv_gem "hermes" do
+#   source "#{node['gitolite']['admin_home']}/build/hermes/chaos_hermes-0.0.1.gem"
+#   version "0.0.1"
+#   action :install
+# end
 
-# Allow git user to manage nginx routes
-directory "#{node['gitolite']['admin_home']}/routes" do
-  user "git"
-  group "git"
-  action :create
-end
-template "hermes sudo conf" do
-  path "/etc/sudoers.d/hermes"
-  source "sudo-hermes"
-  owner "root"
-  group "root"
-  mode "0440"
-  action :create
-end
+# # Allow git user to manage nginx routes
+# directory "#{node['gitolite']['admin_home']}/routes" do
+#   user "git"
+#   group "git"
+#   action :create
+# end
+# template "hermes sudo conf" do
+#   path "/etc/sudoers.d/hermes"
+#   source "sudo-hermes"
+#   owner "root"
+#   group "root"
+#   mode "0440"
+#   action :create
+# end
 
-# Allow git user to manage app processes
-template "deploy sudo conf" do
-  path "/etc/sudoers.d/deploy"
-  source "sudo-deploy"
-  owner "root"
-  group "root"
-  mode "0440"
-  action :create
-end
+# # Allow git user to manage app processes
+# template "deploy sudo conf" do
+#   path "/etc/sudoers.d/deploy"
+#   source "sudo-deploy"
+#   owner "root"
+#   group "root"
+#   mode "0440"
+#   action :create
+# end
 
-# Create the nginx config for chaos
-template "chaos.conf" do
-  path "#{node['nginx']['dir']}/conf.d/chaos.conf"
-  source "chaos.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
-  notifies :reload, 'service[nginx]'
-end
+# # Create the nginx config for chaos
+# template "chaos.conf" do
+#   path "#{node['nginx']['dir']}/conf.d/chaos.conf"
+#   source "chaos.conf.erb"
+#   owner "root"
+#   group "root"
+#   mode 0644
+#   notifies :reload, 'service[nginx]'
+# end
 
-# Install buildpacks
-# Supported:
-# * ruby (https://github.com/heroku/heroku-buildpack-ruby.git)
-directory "#{node['chaos']['buildpacks']['dir']}" do
-  mode 0755
-  action :create
-end
-git "ruby buildpack" do
-  repository "https://github.com/heroku/heroku-buildpack-ruby.git"
-  destination "#{node['chaos']['buildpacks']['dir']}/ruby"
-  action :checkout
-end
+# # Install buildpacks
+# # Supported:
+# # * ruby (https://github.com/heroku/heroku-buildpack-ruby.git)
+# directory "#{node['chaos']['buildpacks']['dir']}" do
+#   mode 0755
+#   action :create
+# end
+# git "ruby buildpack" do
+#   repository "https://github.com/heroku/heroku-buildpack-ruby.git"
+#   destination "#{node['chaos']['buildpacks']['dir']}/ruby"
+#   action :checkout
+# end
