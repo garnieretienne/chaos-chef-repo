67caf6f7dce6280cdf06ffca08275530f366a7ba
Etienne Garnier <garnier.etienne@gmail.com>
2013-04-15 18:48:57 +0200

    import cookbooks and roles from servicepacks

--- solo.rb
+++ solo.rb
@@ -1,2 +1,12 @@
-cookbook_path ["#{File.dirname(__FILE__)}/cookbooks", "#{File.dirname(__FILE__)}/site-cookbooks"]
-role_path "#{File.dirname(__FILE__)}/roles"
+servicepacks_cookbooks = []
+servicepacks_roles     = []
+
+Dir.foreach "" do |servicepack|
+  if File.directory?(servicepack) && servicepack != "." && servicepack != ".." && File.directory?("#{servicepack}/.git")
+    servicepacks_cookbooks << "#{servicepack}/chef/cookbooks"
+    servicepacks_roles << "#{servicepack}/chef/roles"
+  end
+end
+
+cookbook_path [ "#{File.dirname(__FILE__)}/cookbooks", "#{File.dirname(__FILE__)}/site-cookbooks" ] + servicepacks_cookbooks
+role_path [ "#{File.dirname(__FILE__)}/roles" ] + servicepacks_roles
