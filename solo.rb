f0bf4a3e3ea847caa1a97b9929cf288f6e8075b1
Etienne Garnier <garnier.etienne@gmail.com>
2013-04-16 22:49:01 +0200

    fix bad path

--- solo.rb
+++ solo.rb
@@ -4,7 +4,7 @@ servicepacks_cookbooks = []
 if Dir.exist? servicepacks_path 
   Dir.foreach servicepacks_path do |servicepack|
     if File.directory?("#{servicepacks_path}/#{servicepack}") && servicepack != "." && servicepack != ".." && File.directory?("#{servicepacks_path}/#{servicepack}/.git")
-      servicepacks_cookbooks << "#{servicepacks_path}/#{servicepack}/chef/cookbooks"
+      servicepacks_cookbooks << "#{servicepacks_path}/#{servicepack}/vendor/chef/cookbooks"
     end
   end
 end
